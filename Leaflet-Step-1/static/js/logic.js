80c5038b1a3cb139eadee84cafb94a14eb367fb6
jared.roussel@outlook.com <jared.roussel@outlook.com>
2021-11-12 09:54:16 -0500

    Updated code

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -10,33 +10,30 @@ d3.json(geojsonUrl).then(function (data) {
   function createFeatures(earthquakeData) {
   //Define function to run for every feature
   //Create popups
-  function onEachFeature(features, layer) {
-    layer.bindPopup(`<h3>${features.properties.place}</h3><hr><p>Time: ${new Date(features.properties.time)}</p><p>Magnitude: ${features.properties.mag}</p><p>Depth: ${features.geometry.coordinates[2]}</p>`);
-  }
-
-  var earthquakes = L.geoJSON(earthquakeData, {
+    function onEachFeature(features, layer) {
+        layer.bindPopup(`<h3>${features.properties.place}</h3><hr><p>Time: ${new Date(features.properties.time)}</p><p>Magnitude: ${features.properties.mag}</p><p>Depth: ${features.geometry.coordinates[2]}</p>`);
+    }
+  
+    var earthquakes = L.geoJSON(earthquakeData, {
     onEachFeature: onEachFeature
-  });
+    });
 
   createMap(earthquakes);
 }
 
-
-
-  function createMap(earthquakes) {
+function createMap(earthquakes) {
 
     // Create the base layers.
-    var title = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
   
-    // Create a baseMaps object.
+    // Create a baseMaps object
     var baseMaps = {
-      "Earthquakes": street,
-      "Topographic Map": topo
+      "Street Map": street,
     };
   
-    // Create an overlay object to hold our overlay.
+    // Create an overlay object to hold earthquakes
     var overlayMaps = {
       Earthquakes: earthquakes
     };
@@ -45,12 +42,13 @@ d3.json(geojsonUrl).then(function (data) {
     var myMap = L.map("map", {
       center: [45.52, -122.67],
       zoom: 5,
+      layers: [street, earthquakes]
     });
   
     // Create a layer control.
     // Pass it our baseMaps and overlayMaps.
     // Add the layer control to the map.
-    L.control.layers(baseMaps, overlayMaps{
+    L.control.layers(baseMaps, overlayMaps, {
       collapsed: false
     }).addTo(myMap);
   
