d08e5a6f4942a5f02ebcdeab083c6c79b565b0db
jared.roussel@outlook.com <jared.roussel@outlook.com>
2021-11-12 22:48:55 -0500

    Code won't render

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -4,23 +4,51 @@ var geojsonUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_
 
 //Use d3 to make request for features
 d3.json(geojsonUrl).then(function (data) {
-    createFeatures(data.features);
-});
+  
 
- //Define function to run for every feature
-function createFeatures(earthquakeData) {
-    //Create popups
-      function onEachFeature(features, layer) {
-          layer.bindPopup(`<h3>${features.properties.place}</h3><hr><p>Time: ${new Date(features.properties.time)}</p><p>Magnitude: ${features.properties.mag}</p><p>Earthquake Depth: ${features.geometry.coordinates[2]}</p>`);
+  function circleStyle(features) {
+    return {
+      radius: radSize(features.properties.mag),
+      fillColor: fillColor(features.properties.mag),
+      color: "#000000",
+      weight: 1,
+      stroke: true
+      opacity: 1,
+      fillOpacity: 1
+    };
+  
+  
+    function fillColor(mag) {
+      switch(true) {
+      case mag > 5: return "#ea2c2c";
+      case mag > 4: return "#ea822c";
+      case mag > 3: return "#ee9c00";
+      case mag > 2: return "#eecc00";
+      case mag > 1: return "#d4ee00";
+      default: return "#98ee00";
+    }
+  }
+    function radSize(mag) {
+      if (mag === 0) {
+        return 1;
       }
-    
+      return mag * 4;
+  }
+}
+  
       //Contains geojson features
       var earthquakes = L.geoJSON(earthquakeData, {
-      onEachFeature: onEachFeature
+        onEachFeature: function onEachFeature(features, layer) {
+          layer.bindPopup(`<h3>${features.properties.place}</h3><hr><p>Time: ${new Date(features.properties.time)}</p><p>Magnitude: ${features.properties.mag}</p><p>Earthquake Depth: ${features.geometry.coordinates[2]}</p>`);
+        },
+        pointToLayer: function pointToLayer (features, latlng) {
+          return L.circleMarker(latlng);
+            },
+        style: circleStyle,
       });
       
       createMap(earthquakes);
-}
+};
 
 
 function createMap(earthquakes) {
@@ -28,29 +56,28 @@ function createMap(earthquakes) {
     // Create the base layers.
     var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
+    })
   
     // Create a baseMaps object
     var baseMaps = {
       "Street Map": street,
-    };
+    }
   
     // Create an overlay object to hold earthquakes
     var overlayMaps = {
-      Earthquakes: earthquakes
+      "Earthquakes": earthquakes
 
-    };
+    }
   
     //Create map object
     var myMap = L.map("map", {
         center: [45.52, -122.67],
         zoom: 5,
         layers: [street, earthquakes]
-    });
+    })
 
-    // Create a layer control.
-    // Pass it our baseMaps and overlayMaps.
-    // Add the layer control to the map.
+
+    // Add the layers control to the map.
     L.control.layers(baseMaps, overlayMaps, {
       collapsed: true
     }).addTo(myMap);
